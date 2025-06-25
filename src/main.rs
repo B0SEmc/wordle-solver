62dd566dce2513f1758e17ac07cc37dea3bc601e
Liloche19 <lilian.locher@epitech.eu>
2025-06-25 16:25:03 +0200

    [+] error handling improved (pattern must be 5 letters long)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{
     io::{Write, stdin, stdout},
 };
 
+const WORDLE_LENGTH: usize = 5;
+
 fn load_words(path: &str) -> Vec<Vec<char>> {
     let mut words = vec![];
     read_to_string(path)
@@ -18,10 +20,17 @@ fn read_user_input(msg: &str) -> Vec<char> {
     stdout().flush().unwrap();
     stdin().read_line(&mut input).unwrap();
     input = input.trim().to_string();
+    if input.len() != WORDLE_LENGTH && !input.is_empty() {
+        eprintln!("The pattern must be {} characters long!", WORDLE_LENGTH);
+        std::process::exit(1);
+    }
     input.chars().map(|c| c.to_ascii_uppercase()).collect()
 }
 
 fn is_correct_pattern(word: &[char], pattern: &[char]) -> bool {
+    if pattern.is_empty() {
+        return true;
+    }
     for (i, c) in word.iter().enumerate() {
         if pattern[i] != '*' && pattern[i] != *c {
             return false;
@@ -53,12 +62,12 @@ fn main() {
     let pattern = read_user_input("Enter the pattern (t*Up*): ");
     dictionnary.retain(|word| is_correct_pattern(word, &pattern));
     if dictionnary.is_empty() {
-        eprintln!("No word found with this configuration !");
+        eprintln!("No word found with this configuration!");
         std::process::exit(1);
     }
     if dictionnary.len() == 1 {
         println!(
-            "The only solution is : {}",
+            "The only solution is: {}",
             dictionnary[0].iter().collect::<String>()
         );
         return;
@@ -66,12 +75,12 @@ fn main() {
     let missplaced = read_user_input("Enter missplaced letters (aBc): ");
     dictionnary.retain(|word| contain_all_missplaced(word, &missplaced));
     if dictionnary.is_empty() {
-        eprintln!("No word found with this configuration !");
+        eprintln!("No word found with this configuration!");
         std::process::exit(1);
     }
     if dictionnary.len() == 1 {
         println!(
-            "The only solution is : {}",
+            "The only solution is: {}",
             dictionnary[0].iter().collect::<String>()
         );
         return;
@@ -79,12 +88,12 @@ fn main() {
     let invalid_letters = read_user_input("Enter invalid letters (AbC): ");
     dictionnary.retain(|word| does_not_contain_invalid_letters(word, &invalid_letters));
     if dictionnary.is_empty() {
-        eprintln!("No word found with this configuration !");
+        eprintln!("No word found with this configuration!");
         std::process::exit(1);
     }
     if dictionnary.len() == 1 {
         println!(
-            "The only solution is : {}",
+            "The only solution is: {}",
             dictionnary[0].iter().collect::<String>()
         );
         return;
