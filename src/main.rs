c330991d43329e6a80a5051f123e7c27fa07fd3d
thibaut <thibaut.musslin@epitech.eu>
2025-06-25 14:16:51 +0200

    Add load_words function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::fs::read_to_string;
+
+fn load_words(path: &str) -> Vec<Vec<char>> {
+    let mut words = vec![];
+    read_to_string(path)
+        .unwrap()
+        .lines()
+        .for_each(|i| words.push(i.chars().collect()));
+    words
+}
+
 fn main() {
-    todo!("Hello World!");
+    dbg!(load_words("words.txt"));
 }
