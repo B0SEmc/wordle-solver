e9254b1427843dbc1db695fa51ab90c3d3f38e11
Liloche19 <lilian.locher@epitech.eu>
2025-06-27 09:56:08 +0200

    [+] error handling improved on input of missplaced letters

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::{
 };
 
 const WORDLE_LENGTH: usize = 5;
+const DICTIONARY: &str = "./src/words.txt";
 
 fn load_words(path: &str) -> Vec<Vec<char>> {
     let mut words = vec![];
@@ -63,12 +64,12 @@ fn does_not_contain_invalid_letters(word: &[char], invalid_letters: &[char]) -> 
 
 fn check_end(dictionnary: &[Vec<char>]) {
     if dictionnary.is_empty() {
-        eprintln!("No word found with this configuration!");
+        eprintln!("\nNo word found with this configuration!");
         std::process::exit(1);
     }
     if dictionnary.len() == 1 {
         println!(
-            "The only solution is: {}",
+            "\nThe only solution is: {}",
             dictionnary[0].iter().collect::<String>()
         );
         std::process::exit(0);
@@ -76,21 +77,28 @@ fn check_end(dictionnary: &[Vec<char>]) {
 }
 
 fn main() {
-    let mut dictionnary = load_words("./src/words.txt");
-    let pattern = read_user_input("Enter the pattern (t*Up*): ");
+    let mut dictionnary = load_words(DICTIONARY);
+    let pattern = read_user_input("Enter the pattern (t*Up* / nothing): ");
     if pattern.len() != WORDLE_LENGTH && !pattern.is_empty() {
-        eprintln!("The pattern must be {} characters long!", WORDLE_LENGTH);
+        eprintln!("\nThe pattern must be {} characters long!", WORDLE_LENGTH);
         std::process::exit(1);
     }
     dictionnary.retain(|word| is_correct_pattern(word, &pattern));
     check_end(&dictionnary);
-    let mut user_line = read_user_input("Enter missplaced letters (*e***): ");
+    let mut user_line = read_user_input("Enter missplaced letters (*e*** / nothing): ");
     let mut missplaced = vec![];
     while !user_line.is_empty() {
+        if user_line.len() != WORDLE_LENGTH && !user_line.is_empty() {
+            eprintln!(
+                "\nThe missplaced letters pattern must be {} characters long!",
+                WORDLE_LENGTH
+            );
+            std::process::exit(1);
+        }
         missplaced.push(user_line);
         dictionnary.retain(|word| contain_all_missplaced(word, &missplaced));
         check_end(&dictionnary);
-        user_line = read_user_input("Enter missplaced letters (*e***): ");
+        user_line = read_user_input("Enter missplaced letters (*e*** / nothing): ");
     }
     let invalid_letters = read_user_input("Enter invalid letters (AbC): ");
     dictionnary.retain(|word| does_not_contain_invalid_letters(word, &invalid_letters));
